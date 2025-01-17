59fb22c8f47701e78f5fd2fd58a5a3e49b9e9823
Magickio <dad2dadad@gmail.com>
2025-01-17 01:20:43 +0000

    Creamos el código necesario para crear el ExchangeRate entre dos Currency

--- src/main/java/software/ulpgc/moneycalculator/fixerws/FixerExchangeRateLoader.java
+++ src/main/java/software/ulpgc/moneycalculator/fixerws/FixerExchangeRateLoader.java
@@ -2,9 +2,9 @@ package software.ulpgc.moneycalculator.fixerws;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import software.ulpgc.moneycalculator.Control.ExchangeRateLoader;
 import software.ulpgc.moneycalculator.Model.Currency;
 import software.ulpgc.moneycalculator.Model.ExchangeRate;
+import software.ulpgc.moneycalculator.Control.ExchangeRateLoader;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +14,24 @@ import java.time.LocalDate;
 public class FixerExchangeRateLoader implements ExchangeRateLoader {
     @Override
     public ExchangeRate load(Currency from, Currency to) {
-        return null;
+        try {
+            return loadJson(from, to);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private ExchangeRate loadJson(Currency from, Currency to) throws IOException {
+        URL url = new URL("https://data.fixer.io/api/latest?access_key=" + FixerAPI.key);
+        try (InputStream is = url.openStream()) {
+            String result = new String(is.readAllBytes());
+            JsonObject asJsonObject = JsonParser.parseString(result).getAsJsonObject();
+            JsonObject rates = asJsonObject.get("rates").getAsJsonObject();
+
+            return new ExchangeRate(from, to, LocalDate.now(), calcu(rates.get(from.code()).getAsString(), rates.get(to.code()).getAsString()));
+        }
+    }
+
+    private double calcu(String from, String to) {
+        return Double.parseDouble(to)/Double.parseDouble(from);
     }
 }
